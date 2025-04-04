18cf95e77095ad029d8203e8573b00d0bf1c0e57
HyKo <punk.flag8609@fastmail.com>
2025-04-03 22:42:25 -0400

    Update version number to 1.0.6 in SiteMail plugin header

--- sitemail.php
+++ sitemail.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SiteMail
  * Description: Replace WordPress email function with SiteMail API
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: ACARY
  * Author URI: https://acary.ca
  * Text Domain: sitemail
