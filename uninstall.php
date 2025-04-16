4f2ea9e7c9cd710daa5ba2f76b02b9e79537ef67
HyKo <punk.flag8609@fastmail.com>
2025-04-16 19:41:26 -0400

    Enhance uninstall process by deleting all plugin options in SiteMail
    - Added deletion of additional plugin options during uninstallation to ensure complete removal of settings.

--- uninstall.php
+++ uninstall.php
@@ -10,5 +10,13 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
-// Delete the plugin options
-delete_option('sitemail_api_key'); 
+// Delete all plugin options
+delete_option('sitemail_api_key');
+delete_option('sitemail_mailer_type');
+delete_option('sitemail_sender_name');
+delete_option('sitemail_smtp_host');
+delete_option('sitemail_smtp_port');
+delete_option('sitemail_smtp_username');
+delete_option('sitemail_smtp_password');
+delete_option('sitemail_smtp_encryption');
+delete_option('sitemail_smtp_from_email'); 
